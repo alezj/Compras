ab556b40fc79ac391dd7c133fc11dc30ea9c8290
MDSOFT <MDSOFT@DESKTOP-N76UMB6>
2022-06-27 14:17:16 -0400

    Lista de usuarios en la web minuto 15 del video 13

---
+++ Compras/Compras/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using Compras.Datos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Compras.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class UsersController : Controller
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Users
+                .Include(u => u.City)
+                .ThenInclude(c => c.State)
+                .ThenInclude (s => s.Country)
+                .ToListAsync());
+        }
+
+    }
+}
