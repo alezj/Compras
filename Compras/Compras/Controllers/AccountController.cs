0721e6042e144c10a439f71074433f7c701162f4
Alezj <thebrother2011@hotmail.com>
2022-07-03 14:35:35 -0400

    Bloquear usuario

--- Compras/Compras/Controllers/AccountController.cs
+++ Compras/Compras/Controllers/AccountController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Threading.Tasks;
+using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
 
 namespace Compras.Controllers
 {
@@ -42,16 +43,23 @@ namespace Compras.Controllers
         {
             if (ModelState.IsValid)
             {
-                Microsoft.AspNetCore.Identity.SignInResult result = await _userHelper.LoginAsync(model);
+                SignInResult result = await _userHelper.LoginAsync(model);
                 if (result.Succeeded)
                 {
                     return RedirectToAction("Index", "Home");
                 }
+                if (result.IsLockedOut)
+                {
+                    ModelState.AddModelError(string.Empty, "Ha superado el máximo número de intentos, su cuenta está bloqueada, intente de nuevo en 5 minutos.");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Email o contraseña incorrectos.");
+                }
 
-                ModelState.AddModelError(string.Empty, "Email o contraseña incorrectos.");
             }
-               
-           return View(model);
+
+            return View(model);
         }
 
         public async Task<IActionResult> Logout()
