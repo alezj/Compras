d32f5ae284a0515238245a4ed5423a1f5333284f
Alezj <thebrother2011@hotmail.com>
2022-09-28 23:01:40 -0400

    peda

---
+++ Compras/Compras/Controllers/PruebasController.cs
@@ -0,0 +1,36 @@
+﻿using Compras.Datos.Entities;
+using Compras.Helpers;
+using Compras.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Compras.Controllers
+{
+   
+    public class PruebasController :Controller  
+    {
+        private readonly IUserHelper _userHelper;
+        public PruebasController(IUserHelper userHelper)
+
+        {
+            _userHelper = userHelper;
+        }
+        public async Task<IActionResult> LoginPrueba()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                await _userHelper.LogoutAsync();
+                return RedirectToAction("LoginPrueba", "Pruebas");
+            }
+
+            return View(new LoginViewModel());
+
+        }
+        public async Task<IActionResult> Logout()
+        {
+            await _userHelper.LogoutAsync();
+            return RedirectToAction("Login", "Account");
+        }
+    }
+
+ 
+}
